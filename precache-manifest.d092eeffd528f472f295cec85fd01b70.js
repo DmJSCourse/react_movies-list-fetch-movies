aba69059a9c627f7a20da17617a11f192e55ff34
DmJSCourse <dimonua@gmail.com>
2020-09-17 01:54:19 +0300

    Updates

---
+++ precache-manifest.d092eeffd528f472f295cec85fd01b70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d197d2a7092ee01d37ce2aae70668377",
+    "url": "/react_movies-list-fetch-movies/index.html"
+  },
+  {
+    "revision": "c20cc560d1c120803bc6",
+    "url": "/react_movies-list-fetch-movies/static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "499456f86f16ca4e0e38",
+    "url": "/react_movies-list-fetch-movies/static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "c20cc560d1c120803bc6",
+    "url": "/react_movies-list-fetch-movies/static/js/2.c13ea62d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_movies-list-fetch-movies/static/js/2.c13ea62d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "499456f86f16ca4e0e38",
+    "url": "/react_movies-list-fetch-movies/static/js/main.168cfb9e.chunk.js"
+  },
+  {
+    "revision": "6449e3c728d279592b03",
+    "url": "/react_movies-list-fetch-movies/static/js/runtime-main.2ac049b9.js"
+  }
+]);
